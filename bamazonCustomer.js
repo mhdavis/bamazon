5c337a67bf733d20b30e003652e711016f02fa50
Michael Davis <mhdavis1993@gmail.com>
2017-07-31 00:36:04 -0400

    built out connection query to print out products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,6 +8,18 @@ connection.connect(function (err) {
   if (err) {
     throw err;
   } else {
-    console.log("Successfully Connected!");
+    connection.query("SELECT * FROM products", function (err, res) {
+      if (err) throw err;
+      for (let i=0; i <res.length; i++) {
+        console.log(
+          `\n----------------------------- ITEM ${res[i].item_id}\n` +
+          '|| Product Name: ' + res[i].product_name + "\n" +
+          '|| Department: ' + res[i].department_name + "\n" +
+          '|| Price: ' + res[i].price + "\n" +
+          '|| Stock: ' + res[i].stock_quantity + "\n" +
+          "-----------------------------"
+        )
+      }
+    });
   }
 });
