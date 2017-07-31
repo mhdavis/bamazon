a524cb305088a2420de5a0af93921597af1a8d70
Michael Davis <mhdavis1993@gmail.com>
2017-07-31 01:18:11 -0400

    Added customerProcess continuation function
    - Function takes user inputs of desired product id and amount
      and queries DB to determine if specified quantity exceeds stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,17 +9,48 @@ connection.connect(function (err) {
     throw err;
   } else {
     connection.query("SELECT * FROM products", function (err, res) {
+      const customerTotal = 0;
       if (err) throw err;
-      for (let i=0; i <res.length; i++) {
+      for (let i=0; i < res.length; i++) {
         console.log(
-          `\n----------------------------- ITEM ${res[i].item_id}\n` +
-          '|| Product Name: ' + res[i].product_name + "\n" +
-          '|| Department: ' + res[i].department_name + "\n" +
-          '|| Price: ' + res[i].price + "\n" +
+          "\n" +
+          `-------------------------------- ITEM ${res[i].item_id}\n` +
+          '|| Product: ' + res[i].product_name + "\n" +
+          '|| Depart.: ' + res[i].department_name + "\n" +
+          '|| Price: $' + res[i].price.toFixed(2) + "\n" +
           '|| Stock: ' + res[i].stock_quantity + "\n" +
-          "-----------------------------"
-        )
+          "--------------------------------"
+        );
       }
+      customerProcess();
     });
   }
 });
+
+function customerProcess() {
+  console.log("\n");
+  inquirer.prompt([
+    {
+      name: "idSelected",
+      type: "input",
+      message: "Product ID for purchase:"
+    },
+    {
+      name: "quantitySelected",
+      type: "input",
+      message: "Desired quantity for purchase:"
+    }
+  ]).then(function (answer) {
+    connection.query("SELECT * FROM products", function (err, res) {
+      let parsedId = parseInt(answer.idSelected) - 1;
+      let storeStock = res[parsedId].stock_quantity;
+      let desiredAmount = answer.quantitySelected;
+      console.log("");
+      if (storeStock >= desiredAmount) {
+        console.log("There is enough!");
+      } else {
+        console.log("Insufficient Quantity!");
+      }
+    });
+  });
+}
