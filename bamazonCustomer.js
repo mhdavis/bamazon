8429da4dbb1220a71209cbcf26d5fda2e6f82b14
Michael Davis <mhdavis1993@gmail.com>
2017-07-31 20:49:32 -0400

    Finished removeFromStock function
    - Wrote continueOrStop to prompt user whether they
      want to continue shoping or stop
    - Modified initial actions of connection.connect
      by transfering functionality to commenceShop function
    - Added validation to processOrder function's inquirer prompt
    - Fixed conditional in processOrder's inquirer promise
      To allow for user error

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,34 +3,37 @@ const inquirer = require("inquirer");
 const serverParams = require("./config.js");
 
 let connection = mysql.createConnection(serverParams);
+let customerTotal = 0;
 
-connection.connect(function (err) {
+connection.connect(function(err) {
   if (err) {
     throw err;
   } else {
-    connection.query("SELECT * FROM products", function (err, res) {
-      const customerTotal = 0;
-      if (err) throw err;
-      for (let i=0; i < res.length; i++) {
-        console.log(
-          "\n" +
-          `-------------------------------- ITEM ${res[i].item_id}\n` +
-          '|| Product: ' + res[i].product_name + "\n" +
-          '|| Depart.: ' + res[i].department_name + "\n" +
-          '|| Price: $' + res[i].price.toFixed(2) + "\n" +
-          '|| Stock: ' + res[i].stock_quantity + "\n" +
-          "--------------------------------"
-        );
-      }
-      processOrder();
-    });
+    commenceShop();
   }
 });
 
+function commenceShop() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    for (let i = 0; i < res.length; i++) {
+      console.log(
+        "\n" +
+        `-------------------------------- ITEM ${res[i].item_id}\n` +
+        '|| Product: ' + res[i].product_name + "\n" +
+        '|| Depart.: ' + res[i].department_name + "\n" +
+        '|| Price: $' + res[i].price.toFixed(2) + "\n" +
+        '|| Stock: ' + res[i].stock_quantity + "\n" +
+        "--------------------------------"
+      );
+    }
+    processOrder();
+  });
+}
+
 function processOrder() {
   console.log("\n");
-  inquirer.prompt([
-    {
+  inquirer.prompt([{
       name: "idSelected",
       type: "input",
       message: "Product ID for purchase:"
@@ -38,27 +41,58 @@ function processOrder() {
     {
       name: "quantitySelected",
       type: "input",
-      message: "Desired quantity for purchase:"
+      message: "Desired quantity for purchase:",
+      validate: function(value) {
+        if (parseInt(value) >= 0) {
+          return true;
+        }
+        return false;
+      }
     }
-  ]).then(function (answer) {
-    connection.query("SELECT * FROM products", function (err, res) {
-      console.log("");
+  ]).then(function(answer) {
+    connection.query("SELECT * FROM products", function(err, res) {
       let parsedId = parseInt(answer.idSelected);
       let idIndex = parsedId - 1;
-
-      if (!res[idIndex] === undefined) {
+      if (res[idIndex]) {
         let storeStock = res[idIndex].stock_quantity;
         let desiredAmount = answer.quantitySelected;
 
         if (storeStock >= desiredAmount) {
-          console.log("There is enough!");
+          customerTotal += parseFloat(res[idIndex].price) * parseFloat(answer.quantitySelected);
+          removeFromStock(answer);
+          console.log("\nTotal Cost = $" + String(customerTotal.toFixed(2)) + "\n");
+          continueOrStop();
         } else {
-          console.log("Insufficient Quantity!");
+          console.log(`\nProduct ID ${answer.idSelected}: Insufficient Quantity!`);
+          processOrder();
         }
       } else {
         console.log(`\nSorry! No Product with ID: [ ${answer.idSelected} ] Found!`);
         processOrder();
       }
     });
+  });
+}
+
+function removeFromStock(ans) {
+  let rfsQuery = `UPDATE products SET stock_quantity=stock_quantity-${parseInt(ans.quantitySelected)} WHERE item_id=${parseInt(ans.idSelected)}`;
+  connection.query(rfsQuery, function(err) {
+    if (err) throw err;
+  });
+}
+
+function continueOrStop() {
+  inquirer.prompt([{
+    name: "continue",
+    type: "confirm",
+    message: "Would you like to continue shopping?",
+    default: true
+  }]).then(function(reply) {
+    if (reply.continue) {
+      commenceShop();
+    } else {
+      console.log("\nFinal Total Cost = $" + String(customerTotal.toFixed(2)) + "\n");
+      console.log("--- THANK YOU FOR SHOPPING, HAVE A NICE DAY! ---")
+    }
   });
 }
